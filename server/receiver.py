3dae827d4ccfbe2d8ec1ca8b4638e0c9e53fbb17
Élie Bouttier <cube.elie@gmail.com>
2012-09-06 01:18:03 +0200

    Utilisation du ldap pour les n° de tél

--- server/receiver.py
+++ server/receiver.py
@@ -6,6 +6,26 @@ import socketserver
 import urllib.parse
 from settings import INPUT_PORT
 import select, errno
+from os.path import isfile
+import pickle
+
+
+class Phone:
+	@classmethod
+	def to_name(cls, phone):
+		if not hasattr(cls, 'dico'):
+			if isfile('phonedump'):
+				with open('phonedump', 'rb') as f:
+					cls.dico = pickle.load(f)
+			else:
+				cls.dico = {}
+
+		try:
+			name = cls.dico[phone]
+		except KeyError:
+			name = phone
+
+		return name
 
 
 class SMSHandler(BaseHTTPRequestHandler):
@@ -14,10 +34,12 @@ class SMSHandler(BaseHTTPRequestHandler):
         params = urllib.parse.parse_qs(o.query)
         if 'phone' in params and 'text' in params:
             self.send_response(200)
+            phone = Phone.to_name(params['phone'][0])
+            message = params['text'][0]
             if 'moderate' in params and params['moderate'][0] == 'no':
-                self.send(params['phone'][0], params['text'][0])
+                self.send(phone, message)
             else:
-                self.moderate(params['phone'][0], params['text'][0])
+                self.moderate(phone, message)
         else:
             self.send_response(412)
 
